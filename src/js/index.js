59112d4c5fdb2f2ddad2cde01e724c71ec0c1a24
Ben Hsieh <bqh5026@gmail.com>
2020-04-27 21:46:47 -0400

    Add query variable for more search flexibilities

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,26 @@
 import Search from './models/Search'; 
+import * as searchView from './views/searchView'
+import { elements } from './views/base'
 
 const state = {}; 
 
-const search = new Search('pizza'); 
-search.getResults(); 
+const constrolSearch = async () => {
+    // const query = 'pizza'; 
+    const query = searchView.getInput();
+    console.log(query);
+
+    if (query) {
+        state.search = new Search(query); 
+        await state.search.getResults(); 
+        console.log(state.search.result); 
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    constrolSearch();
+});
+
+// const search = new Search('pizza'); 
+// search.getResults(); 
 console.log(search); 
